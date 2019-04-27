dd1c9ca7056620a55f83f2c475393e3e14504127
Administrator <administrator@gbtc408001dt.montgomerycollege.edu>
2019-04-27 11:07:07 -0400

    First Commit

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
 
 }
