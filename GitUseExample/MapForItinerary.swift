8d74df72551904c222f59e54e40f505ec82d0cee
Administrator <administrator@gbtc408001dt.montgomerycollege.edu>
2019-04-27 11:16:49 -0400

    First Branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by MC Student on 4/27/19.
+//  Copyright © 2019 MC Student. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
